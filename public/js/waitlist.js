3b462a34d0dd84bd3067ecf9e0afd7ca53d94d75
Ashpan Raskar <Ashpanr@gmail.com>
2020-07-09 19:51:39 -0400

    added anon users

--- public/js/waitlist.js
+++ public/js/waitlist.js
@@ -106,11 +106,34 @@ searchStore = async (db,email) => {
 }
 
 betaValue = formatBetaAccess(betaAccess.value)
-
+var anonIn = false;
 submit.addEventListener("click", (event) => {
   event.preventDefault()
+  
   var submitReady = validateInputs(name.value, email.value, referral.value)
-  if (submitReady){
+  if (submitReady && this.anonIn){
     addToWaitlist(db, name.value, email.value, formatBetaAccess(betaValue))
   }
 })
+
+
+loginAnon = () => {
+    firebase.auth().signInAnonymously().catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode)
+        console.log(errorMessage)
+    });
+}
+
+authChanged = () => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            this.anonIn = true
+        }else{
+            this.anonIn = false
+        }
+    });
+}
