4262c4a65a9703ac655fb78fffc39e17c4eeb45d
Bilal Qadar <qadarbilal@gmail.com>
2020-07-10 13:47:29 -0400

    Fixed scrolling up behaviour

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,13 @@ $("a[href^='#']").click(function(e) {
 	e.preventDefault();
 
 	var position = $($(this).attr("href")).offset().top;
+	var offset = 50;
+
+	if (position === 0){
+		offset = 0
+	}
+
 	$("body, html").animate({
-		scrollTop: position + 50
+		scrollTop: position + offset
 	} /* speed */ );
 });
