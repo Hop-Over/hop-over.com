dba30b201d2476771f4c34ad3cda0fc5b2d4dff3
Bilal Qadar <qadarbilal@gmail.com>
2020-07-19 17:13:10 -0400

    UI tweaks

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ $("a[href^='#']").click(function(e) {
 	e.preventDefault();
 
 	var position = $($(this).attr("href")).offset().top;
-	var offset = 50;
+	var offset = 80;
 
 	if (position === 0){
 		offset = 0
